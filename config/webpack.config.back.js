ae740181f0fe6aee7f36a1f54e6f3e8993cf78bf
Alberto Arana <aaranaboza@gmail.com>
2017-11-08 19:11:40 +0100

    First commit with code

---
+++ config/webpack.config.back.js
@@ -0,0 +1,146 @@
+'use strict'
+
+const autoprefixer = require('autoprefixer')
+const path = require('path')
+const webpack = require('webpack')
+const eslintFormatter = require('react-dev-utils/eslintFormatter')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+const paths = require('./paths')
+const getClientEnvironment = require('./env')
+
+// Source maps are resource heavy and can cause out of memory issue for large source files.
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
+// Get environment variables to inject into our app.
+const env = getClientEnvironment()
+
+// Assert this just to be safe.
+// Development builds of React are slow and not intended for production.
+if (env.stringified['process.env'].NODE_ENV !== '"production"') {
+  throw new Error('Production builds must have NODE_ENV=production.')
+}
+
+
+const nodeModules = {}
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod
+  })
+
+module.exports = {
+  target: 'node',
+  entry: [
+    require.resolve('./polyfills'),
+    paths.appIndexJs
+  ],
+
+  output: {
+    path: paths.appBuild,
+    filename: '[name].[chunkhash:8].js',
+  },
+  externals: nodeModules,
+  resolve: {
+    modules: ['node_modules', paths.appNodeModules].concat(
+      process.env.NODE_PATH.split(path.delimiter)
+        .filter(Boolean)
+    ),
+    extensions: ['.js', '.json'],
+    alias: {},
+    plugins: [
+      new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+    ],
+  },
+
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // First, run the linter.
+      // It's important to do this before Babel processes the JS.
+      {
+        test: /\.(js)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        include: paths.appSrc,
+      },
+      {
+        // "oneOf" will traverse all following loaders until one will
+        // match the requirements. When no loader matches it will fall
+        // back to the "file" loader at the end of the loader list.
+        oneOf: [
+          // Process JS with Babel.
+          {
+            test: /\.(js)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+
+              compact: true,
+            },
+          }
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        comparisons: false,
+      },
+      output: {
+        comments: false,
+        ascii_only: true,
+      },
+      sourceMap: shouldUseSourceMap,
+    }),
+    new webpack.DefinePlugin(env.stringified),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ],
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: true,
+    __dirname: true,
+  },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
